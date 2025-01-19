9d3e7e291a8711639702e703961ed0086267e3a4
Michael Austin Burch <86203662+BirdieTX@users.noreply.github.com>
2025-01-19 12:47:48 -0600

    Update install-packages.sh
    Testing for Arch

--- install-packages.sh
+++ install-packages.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Script to automatically install packages on Fedora 41
+# Script to automatically install packages on Arch Linux
 # Usage: ./install-packages.sh
 
 # Colors for output
@@ -15,124 +15,20 @@ if [ "$EUID" -ne 0 ]; then
 fi
 
 echo -e "${GREEN}Updating system packages...${RESET}"
-dnf update -y
-
-# Add Brave Browser repository
-echo -e "${GREEN}Installing dnf-plugins-core and adding Brave Browser repository...${RESET}"
-if dnf install -y dnf-plugins-core; then
-    echo -e "${GREEN}dnf-plugins-core installed successfully.${RESET}"
-else
-    echo -e "${RED}Failed to install dnf-plugins-core.${RESET}"
-    exit 1
-fi
-
-if dnf config-manager --add-repo https://brave-browser-rpm-release.s3.brave.com/brave-browser.repo; then
-    echo -e "${GREEN}Brave Browser repository added successfully.${RESET}"
-else
-    echo -e "${RED}Failed to add Brave Browser repository.${RESET}"
-    exit 1
-fi
-
-# Add Codium repository
-echo -e "${GREEN}Adding Codium RPM Keys...${RESET}"
-if rpmkeys --import https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/-/raw/master/pub.gpg; then
-    echo -e "${GREEN}Codium RPM Keys added successfully.${RESET}"
-else
-    echo -e "${RED}Failed to import Codium RPM Keys.${RESET}"
-    exit 1
-fi
-
-if printf "[gitlab.com_paulcarroty_vscodium_repo]\nname=download.vscodium.com\nbaseurl=https://download.vscodium.com/rpms/\nenabled=1\ngpgcheck=1\nrepo_gpgcheck=1\ngpgkey=https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/-/raw/master/pub.gpg\nmetadata_expire=1h\n" | sudo tee -a /etc/yum.repos.d/vscodium.repo; then
-    echo -e "${GREEN}Codium Repository added successfully.${RESET}"
-else
-    echo -e "${RED}Failed to add Codium Repository.${RESET}"
-    exit 1
-fi
-
-# Adding Bibata Cursor repository
-echo -e "${GREEN}Adding Bibata cursor repository...${RESET}"
-if dnf copr enable peterwu/rendezvous; then
-    echo -e "${GREEN}Bibata Cursor Repository added successfully${RESET}"
-else
-    echo -e "${RED}Failed to import Bibata Cursor Repository.${RESET}"
-    exit 1
-fi
+pacman -Syu
 
 # List of packages to install
 PACKAGES=(
-    neovim
-    fastfetch
-    htop
-    btop
-    flatpak
-    brave-browser
-    codium
-    zfs-fuse
-    xhost
-    fish
-    qalculate-gtk
-    libnotify
-    dunst
-    mediawriter
-    nautilus
-    kitty
-    hyprland
-    hyprpaper
-    xdg-desktop-portal-hyprland
-    qt5-qtwayland
-    qt6-qtwayland
-    xdg-desktop-portal-gtk
-    brightnessctl
-    nm-connection-editor
-    gtk4
-    libadwaita
-    blueman
-    qt6ct
-    waybar
-    rofi-wayland
-    pavucontrol
-    papirus-icon-theme
-    bibata-cursor-themes 
-    flatpak
-    NetworkManager-tui
-    grub-customizer
-    simple-scan
-    gnome-characters
-    audacity
-    evince
-    flatseal
-    gimp
-    kdenlive
-    loupe
-    solaar
-    remmina
-    libreoffice-writer
-    libreoffice-calc
-    libreoffice-impress
-    libreoffice-draw
-    libreoffice-math
-    vlc
-    obs-studio
-    "https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm"
-    "https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm"
-
+    "cmatrix 
+    genact"
 )
 
 echo -e "${GREEN}Installing packages...${RESET}"
 for PACKAGE in "${PACKAGES[@]}"; do
-    if dnf install -y "$PACKAGE"; then
+    if pacman -S "$PACKAGE"; then
         echo -e "${GREEN}$PACKAGE installed successfully.${RESET}"
     else
         echo -e "${RED}Failed to install $PACKAGE.${RESET}"
     fi
 done
-
-# Post-installation steps
-echo -e "${GREEN}Setting up Flatpak remote for Flathub...${RESET}"
-flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
-
-echo -e "${GREEN}Cleaning up...${RESET}"
-dnf autoremove -y
-dnf clean all
-
 echo -e "${GREEN}All tasks completed successfully.${RESET}"
